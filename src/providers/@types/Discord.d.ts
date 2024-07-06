fff295b7f17cdcb8822edd7a18b4680165c5c2fa
slxca <straeter.luca@gmail.com>
2024-07-06 22:06:04 +0200

    chore: Update DiscordProvider to use new DiscordScopes enum

--- src/providers/@types/Discord.d.ts
+++ src/providers/@types/Discord.d.ts
@@ -2,7 +2,7 @@ export type DiscordProviderSettings = {
     clientId: string;
     clientSecret: string;
     redirectUri: string;
-    scopes: string[];
+    scopes: DiscordScopes[];
 }
 
 export type DiscordProfile = {
--- src/providers/Discord.ts
+++ src/providers/Discord.ts
@@ -1,7 +1,7 @@
 import {DiscordProfile, DiscordProviderSettings} from "./@types/Discord";
 import axios from "axios";
 
-class DiscordProvider {
+export default class DiscordProvider {
 
     public settings: DiscordProviderSettings;
     
@@ -9,10 +9,20 @@ class DiscordProvider {
         this.settings = settings;
     }
 
+    /**
+     * Returns the authorization URI for Discord OAuth2.
+     * 
+     * @returns The authorization URI.
+     */
     getAuthUri(): string {
         return `https://discord.com/oauth2/authorize?response_type=code&client_id=${this.settings.clientId}&redirect_uri=${this.settings.redirectUri}&scope=${this.settings.scopes.join('%20')}`;
     }
 
+    /**
+     * Retrieves an access token from Discord API using the provided authorization code.
+     * @param code - The authorization code obtained from the user.
+     * @returns The access token.
+     */
     async getAccessToken(code: string) {
         const response = await axios.post("https://discord.com/api/oauth2/token", {
                 client_id: this.settings.clientId,
@@ -30,6 +40,12 @@ class DiscordProvider {
         return response.data.access_token;
     }
 
+    /**
+     * Retrieves the Discord profile information using the provided access token.
+     * @param accessToken - The access token for authentication.
+     * @returns A promise that resolves to the Discord profile information.
+     * @throws An error if failed to get the profile.
+     */
     async getProfile(accessToken: string): Promise<DiscordProfile> {
     
         try {
@@ -62,5 +78,3 @@ class DiscordProvider {
         }
     }
 }
-
-export default DiscordProvider;
